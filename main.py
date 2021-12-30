5e4ffa317864584e1c1f6ecb547d67318cc80286
AltynayLinstrum <altynaylinstrum@yahoo.com>
2021-12-29 17:17:11 -0800

    First iteration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("Welcome to the most Awesome Game!")
-name = input("What's your name? ")
+name = input("What's your name? ").lower()
 age = int(input("How old are you? "))
+health_points = 10
 
 if age <= 20:
     print("Sorry, you are not eligible to play the game")
@@ -8,8 +9,37 @@ if age <= 20:
 else:
     print("First contestant's name is ", name, "and they are", age, "years old!")
 
-wants_to_play = input("Are you ready to play?")
-if wants_to_play == "Yes":
+wants_to_play = input("You have 10 health points! Are you ready to play? ")
+if wants_to_play == "yes":
     print("Let's start our 1st round!")
+    left_or_right = input("Please choose if you'd like to turn left or right! ")
+    if left_or_right == "Left":
+        print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
+        health_points -= 2
+        print("Now you have only", health_points, "point left!")
+        try_again = input("Would you like to try again?")
+        if try_again == "Yes":
+            try_again2 = input("Please choose if you'd like to turn left or right! ")
+            if try_again2 == "Left":
+                print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
+                health_points -= 2
+                print("Now you have only", health_points, "point left!")
+            if try_again2 == "Right":
+                print("Great choice! You found a pot of gold!")
+                health_points += 2
+                print ("Now you have,", health_points, "health points! ")
+
+
+        else: 
+            print("Good Bye!")
+
+    if left_or_right == "Right":
+        print("Great choice! You found a pot of gold!")
+        health_points += 2
+        print ("Now you have,", health_points, "health points! ")
+
 else:
-    print("No problem! Who is our next contestant?")
+    print("No problem, maybe we can play next time!")
+    exit()
+
+
