f7832636812fffade897520a801eba5fef3ff658
AltynayLinstrum <altynaylinstrum@yahoo.com>
2021-12-29 17:53:38 -0800

    Second iteration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Welcome to the most Awesome Game!")
-name = input("What's your name? ").lower()
+name = input("What's your name? ")
 age = int(input("How old are you? "))
 health_points = 10
 
@@ -9,37 +9,28 @@ if age <= 20:
 else:
     print("First contestant's name is ", name, "and they are", age, "years old!")
 
-wants_to_play = input("You have 10 health points! Are you ready to play? ")
-if wants_to_play == "yes":
-    print("Let's start our 1st round!")
-    left_or_right = input("Please choose if you'd like to turn left or right! ")
-    if left_or_right == "Left":
-        print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
+def game_intro():
+    print("You have", health_points)
+
+    wants_to_play = input("Are you ready to play? ")
+    if wants_to_play == "Yes":
+        print("Let's start our 1st round!")
+        game_round1()
+    else:
+        print("No problem, maybe we can play next time!")
+        exit()
+
+
+def game_round1():
+    decision = input("Please select left or right!")
+    if decision == "Left":
         health_points -= 2
-        print("Now you have only", health_points, "point left!")
-        try_again = input("Would you like to try again?")
-        if try_again == "Yes":
-            try_again2 = input("Please choose if you'd like to turn left or right! ")
-            if try_again2 == "Left":
-                print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
-                health_points -= 2
-                print("Now you have only", health_points, "point left!")
-            if try_again2 == "Right":
-                print("Great choice! You found a pot of gold!")
-                health_points += 2
-                print ("Now you have,", health_points, "health points! ")
-
-
-        else: 
-            print("Good Bye!")
-
-    if left_or_right == "Right":
-        print("Great choice! You found a pot of gold!")
+        print("Oh no,", name, "you lost")
+    if decision == "Right":
         health_points += 2
-        print ("Now you have,", health_points, "health points! ")
+        print("You found gold! Now you have", health_points, "health points!")
 
-else:
-    print("No problem, maybe we can play next time!")
-    exit()
+game_intro()
 
-
+def test():
+    print(health_points)
---
+++ test.py
@@ -0,0 +1,26 @@
+
+    # left_or_right = input("Please choose if you'd like to turn left or right! ")
+    # if left_or_right == "Left":
+    #     print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
+    #     health_points -= 2
+    #     print("Now you have only", health_points, "point left!")
+    #     try_again = input("Would you like to try again?")
+    #     if try_again == "Yes":
+    #         try_again2 = input("Please choose if you'd like to turn left or right! ")
+    #         if try_again2 == "Left":
+    #             print("Oh no, there was a big crocodile on the left, and you lose 2 health points!")
+    #             health_points -= 2
+    #             print("Now you have only", health_points, "point left!")
+    #         if try_again2 == "Right":
+    #             print("Great choice! You found a pot of gold!")
+    #             health_points += 2
+    #             print ("Now you have,", health_points, "health points! ")
+
+
+    #     else: 
+    #         print("Good Bye!")
+
+    # if left_or_right == "Right":
+    #     print("Great choice! You found a pot of gold!")
+    #     health_points += 2
+    #     print ("Now you have,", health_points, "health points! ")
